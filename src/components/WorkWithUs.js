197000d4e7a16065af13027b28a55665ed6166da
dhilipanraja-joseph <dlipnraj@gmail.com>
2017-01-27 16:53:11 -0600

    add fonts

--- src/components/WorkWithUs.js
+++ src/components/WorkWithUs.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export default class WorkWithUs extends Component {
   render() {
     return (
-      <h1 className="text-center">WorkWithUs</h1>
+      <div>
+        <h3 className="text-center">Ask Us to build a WebApp <span> (or) </span> Contribute to our WebApp</h3>
+      </div>
     );
   }
 }
