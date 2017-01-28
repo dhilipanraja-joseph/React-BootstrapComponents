ec51ac4cc888e9ff5d7824a7b060319fcd64e7c2
Abiya Ruth Joseph <abiyaruth@gmail.com>
2017-01-27 21:44:14 -0600

    Added whoWeAre

--- src/components/WhyUs.js
+++ src/components/WhyUs.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export default class WhyUs extends Component {
   render() {
     return (
+      <div>
       <h1 className="text-center">WhyUs</h1>
+    </div>
     );
   }
 }
