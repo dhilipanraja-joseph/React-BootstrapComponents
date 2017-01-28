e46cefe635875a67a4ba0f5c40c630ebf10fbb01
dhilipanraja-joseph <dlipnraj@gmail.com>
2017-01-27 18:19:42 -0600

    add state

---
+++ src/components/Contribute.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Contribute extends Component {
+  render() {
+    return (
+      <div>
+        Contribute
+      </div>
+    );
+  }
+}
