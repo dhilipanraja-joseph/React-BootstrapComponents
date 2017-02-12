db1a3dd0413a49c49e778886b9847a79a352b318
Abiya Ruth Joseph <abiyaruth@gmail.com>
2017-02-12 17:39:12 -0600

    Why Us

--- src/components/WhyUs.js
+++ src/components/WhyUs.js
@@ -1,25 +1,34 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+
+const whyImg={
+  background: "url('why1.png') no-repeat center",
+  backgroundSize: "cover",
+  height: "75vh",
+  color: "white",
+};
 
 export default class WhyUs extends Component {
-  componentDidMount() {
-    window.scrollTo(0,0);
-  }
-  render() {
-    return (
-      <div className="container">
-      <h1 className="text-center">Why Us</h1>
-      <ul>
-        <li>Websites will be build with latest technologies</li>
-        <li>Customized website designs which makes your website unique</li>
-        <li>Full featured rich web responsive user interface will be designed</li>
-        <li>Have a website design in your mind,let us make it as a website</li>
-        <li>We provide you the screenshots before designing the website,We wait for your design approval</li>
-        <li>We strive hard to meet your requirements</li>
-        <li>No templates will be used</li>
-        <li>We also do social marketing upon your request</li>
-      </ul>
-      
-    </div>
-    );
-  }
+    componentDidMount() {
+        window.scrollTo(0, 0);
+    }
+    render() {
+        return (
+             <div>
+              <div style={whyImg}>
+              </div>
+              <div className="container" style={{paddingTop:"10px"}}>
+                <ul>
+                    <li>Websites will be build with latest technologies</li>
+                    <li>Customized website designs which makes your website unique</li>
+                    <li>Full featured rich web responsive user interface will be designed</li>
+                    <li>Have a website design in your mind,let us make it as a website</li>
+                    <li>We provide you the screenshots before designing the website,We wait for your design approval</li>
+                    <li>We strive hard to meet your requirements</li>
+                    <li>No templates will be used</li>
+                    <li>We also do social marketing upon your request</li>
+                </ul>
+            </div>
+          </div>
+        );
+    }
 }
