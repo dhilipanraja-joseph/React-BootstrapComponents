ae1071692cb97d6beb6d2af0cf96ae6c1881a11a
Abiya Ruth Joseph <abiyaruth@gmail.com>
2017-02-12 15:27:29 -0600

    What we do

--- src/components/WhoWeAre.js
+++ src/components/WhoWeAre.js
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+
+const Whowe={
+  background: "url('baytown.jpg') no-repeat center",
+  backgroundSize: "cover",
+  height: "90vh",
+  color: "white",
+  paddingTop:"10px",
+};
 
 export default class WhoWeAre extends Component {
-  componentDidMount() {
-    window.scrollTo(0,0);
-  }
-  render() {
-    return (
-      <div className="container">
-      <h1 className="text-center">WhoWeAre</h1>
-      <p>We are a group of developers
-        who are striven to work hard with thirst of learning new technologies,
-        Graduate's in Bachelors degree with majors in Computer Science and Engineering.
-        Looking forward to create websites for you to enhance your buisness online presence .</p>
-    </div>
-    );
-  }
+    componentDidMount() {
+        window.scrollTo(0, 0);
+    }
+    render() {
+        return (
+            <div>
+              <div style={Whowe}>
+
+                <h1 className="text-center">Who We Are</h1>
+              </div>
+              <div className="container" style={{paddingTop:"10px"}}>
+                <p>Website4U is commited to empowering your buisness presence online and to grow your market.We focus on producing the quality web responsive websites. We are a group of developers who are striven to work hard with thirst of learning new technologies, Graduate's in Bachelors degree with majors in Computer Science and Engineering. Looking forward to create websites for you to enhance your buisness online presence.</p>
+                <p>Website4U is headquatered in Baytown,TX</p>
+              </div>
+
+            </div>
+        );
+    }
 }
