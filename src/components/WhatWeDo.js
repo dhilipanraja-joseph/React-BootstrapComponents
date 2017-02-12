faa637c9a81ed6d3a6319bca258f953ebcc930b3
Abiya Ruth Joseph <abiyaruth@gmail.com>
2017-02-11 21:47:33 -0600

    What we do

--- src/components/WhatWeDo.js
+++ src/components/WhatWeDo.js
@@ -5,7 +5,8 @@ const wBox = {
 };
 const WhatDo = {
   color: "white",
-  background: "url('sm.jpg') no-repeat center",
+  paddingTop: "10px",
+  background: "url('sm.jpg') no-repeat center fixed",
   backgroundSize:"cover",
   height: "85vh",
 };
@@ -19,6 +20,15 @@ const BuildWeb={
   height: "90vh",
   color: "white",
 };
+const imgPad={
+  marginTop :"60px",
+  textAlign: "center",
+
+};
+const imgStyle ={
+  border :"2px solid black",
+
+};
 
 export default class WhatWeDo extends Component {
   componentDidMount() {
@@ -27,9 +37,9 @@ export default class WhatWeDo extends Component {
   render() {
     return (
 
-      <div className="container" style={wBox}>
+      <div style={wBox}>
         <div style={WhatDo}>
-        <h1 className="text-center">What We Do</h1>
+        {/* <h1 className="text-center">What We Do</h1> */}
         {/* <p> We build websites for your buisness and design according to your needs.
           We will provide you the sample snapshots of how your website looks and upon your confirmation and verification,we will build you the website you are really exicted to see meeting all your requirements.
          You are free to select the style,colour and the fonts of your needs and we do consider all your requirements including secure online payments,database management but not limited to.Feel free to send all your requirements and we are ready to build a high quality website for you </p> */}
@@ -40,11 +50,13 @@ export default class WhatWeDo extends Component {
           We will provide you the sample snapshots of how your website looks and upon your confirmation and verification,we will build you the website you are really exicted to see meeting all your requirements.
          You are free to select the style,colour and the fonts of your needs and we do consider all your requirements including secure online payments,database management but not limited to.Feel free to send all your requirements and we are ready to build a high quality website for you </p>
          {/* <h1>Build websites</h1> */}
-         <div className="container" style={BuildWeb}>
+         <div style={BuildWeb}>
           <h1>Build Website </h1>
          </div>
-           {/* <img src="bookshare.gif"></img> */}
-      </div>
+         <div style={imgPad}>
+           <img style={imgStyle} src="ezgif.com-optimize.gif" ></img>
+         </div>
+    </div>
       </div></div>
     );
   }
