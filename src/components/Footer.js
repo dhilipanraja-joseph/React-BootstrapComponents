441c2cf5e95b05e349127359f2527ba80925c859
Abiya Ruth Joseph <abiyaruth@gmail.com>
2017-02-14 09:22:15 -0600

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ export default class Footer extends Component {
         <div className="row">
           <div className="col-sm-4"> Copyright &#9400;2017. All Rights Reserved. </div>
           <div className="col-sm-4 text-center"> </div>
-          <div className="col-sm-4 text-right"> Facebook, Twitter </div>
+          <div className="col-sm-4 text-right"> Facebook | Twitter | Instagram</div>
         </div>
       </div>
     );
