799522a255244e8736f3dc93886eb62c1ea21ada
dhilipanraja-joseph <dlipnraj@gmail.com>
2017-02-16 21:15:47 -0600

    add build

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 // ROUTES
 app.use('/api', require('./routes/api'));
