e305156db6588dbb655a55d3012674ed4e6cc0ed
dhilipanraja-joseph <dlipnraj@gmail.com>
2017-03-02 09:30:10 -0600

    change link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ export default class Footer extends Component {
         <div className="row">
           <div className="col-sm-6 text-center"> Copyright &#9400;2017. All Rights Reserved. </div>
           {/* <div className="col-sm-4 text-center"> </div> */}
-          <div className="col-sm-6 text-center"> <a href="https://www.facebook.com/VedasWebSolutions/" target="_blank">Facebook</a> | <a>Twitter</a> | <a>Instagram</a> | <a>Google+</a></div>
+          <div className="col-sm-6 text-center"> <a href="https://www.facebook.com/VedaWebSolutions/" target="_blank">Facebook</a> | <a>Twitter</a> | <a>Instagram</a> | <a>Google+</a></div>
         </div>
       </div>
     );
